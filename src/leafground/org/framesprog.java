5da74bdcc35af981786e465a7aeff7d734d4925d
RATHNARAJESH <rathnarajesh046@gmail.com>
2022-07-24 10:09:47 +0530

    push code

---
+++ src/leafground/org/framesprog.java
@@ -0,0 +1,42 @@
+package leafground.org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class framesprog {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Acer\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/frame.html");
+		driver.manage().window().maximize();
+		
+		Thread.sleep(1000);
+		driver.switchTo().frame(0);
+		
+		Thread.sleep(1000);
+		WebElement button1 = driver.findElement(By.xpath("//button[@id='Click']"));
+		button1.click();
+		
+		driver.switchTo().defaultContent();
+		
+		Thread.sleep(1000);
+		WebElement text = driver.findElement(By.xpath("//h1[text()='Fun with frames']"));
+		String text2 = text.getText();
+		System.out.println(text2);
+		
+		Thread.sleep(1000);
+		driver.switchTo().frame(1);
+		
+		driver.switchTo().frame("frame2");
+		
+		Thread.sleep(1000);
+		WebElement button2 = driver.findElement(By.xpath("//button[@id='Click1']"));
+		button2.click();
+	}
+
+}
